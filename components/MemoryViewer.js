d62368a6619ff72742d12e7d526ada43d083a7fe
Noah Spencer <nbspencer.mail@gmail.com>
2024-01-19 20:35:16 -0700

    adding memoryViewer page

---
+++ components/MemoryViewer.js
@@ -0,0 +1,105 @@
+import React, {useState} from 'react';
+import {View, Text, Image, ScrollView, TextInput, Pressable, StyleSheet} from 'react-native';
+import { useSelector, useDispatch } from 'react-redux';
+import { reviewMemory } from '../redux/memoriesSlice';
+
+const MemoryViewer = ({ setCurrentPage, currentMemory }) => {
+  const [isComplete, setIsComplete] = useState(false);
+  const memory = useSelector((state) => state.memories.find(obj => obj.id === currentMemory));
+
+  const dispatch = useDispatch();
+
+  return (
+    <View style={styles.container}>
+      <ScrollView>
+        <Text style={styles.title}>{memory.title}</Text>
+        <Text style={styles.text}>{memory.text}</Text>
+        {memory.answer != '' && memory.answer != null ? 
+            <TextInput
+                style={styles.answerInput}
+                placeholder="Answer"
+                placeholderTextColor={'#818282'}
+                color={'white'}
+                keyboardAppearance={'dark'}
+                onChangeText={newText => setAnswer(newText)}
+            />
+        : 
+            null
+        }
+        
+        
+      </ScrollView>
+      <View style={styles.buttonFrame}>
+        <Pressable style={styles.completeButton} onPress={() => {
+            setIsComplete(true);
+            dispatch(reviewMemory({ currentMemory }))
+            setCurrentPage('Home')
+          }}>
+          <Text style={styles.completeText}>Complete</Text>
+        </Pressable>
+      </View>
+    </View>
+    
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop:80,
+    backgroundColor: '#05202a',
+    flex: 1,  
+  },
+  title: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 30,
+    textAlign: 'center',
+  },
+  buttonFrame: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    bottom: 40,
+    alignItems: 'center'
+  },
+  completeButton: {
+    height: 60,
+    width: 180,
+    backgroundColor: '#4dcbf7',
+    borderRadius: '100%',
+    shadowColor: 'black',
+    shadowOpacity: .5,
+    shadowOffset: {
+        width: 1,
+        height: 2,
+    },
+    shadowRadius: 10,
+  },
+  completeText: {
+    textAlign: 'center',
+    color: 'white',
+    height: '100%',
+    lineHeight: 60,
+    fontSize: 20,
+    shadowColor: 'black',
+    shadowOpacity: .5,
+    shadowOffset: {
+        width: 1,
+        height: 2, 
+    }
+  },
+  text: {
+    backgroundColor: '#1e2b36',
+    margin: 20,
+    height: 40,
+    padding: 10,
+    color: 'white',
+  },
+  answerInput: {
+    backgroundColor: '#1e2b36',
+    margin: 20,
+    padding: 10,
+  },
+});
+
+export default MemoryViewer;
